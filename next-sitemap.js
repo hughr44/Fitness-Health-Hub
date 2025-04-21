0062bb1b9bdb424faf48b99cc38302aa6adf27ef
Hugh Robertson <hughrobertson.cs@gmail.com>
2025-04-21 13:03:24 -0500

    Update sitemap configuration with correct Vercel URL

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://tempsite.com',
+  siteUrl: 'https://fitness-health-hub.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/404'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://tempsite.com/server-sitemap.xml',
+      'https://fitness-health-hub.vercel.app/server-sitemap.xml',
     ],
     policies: [
       {
